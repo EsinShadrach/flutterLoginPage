460ff4fa52b335650954250b52e82869afde2345
EsinShadrach <esinshadrachrafe@gmail.com>
2023-05-20 09:44:42 +0100

    Created login page, added "useMaterial3"

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple login page 
+
+First Unguided flutter tutorial.
+
+it simply Just prints out the email and password passed into it
+<h2 align="center">How It looks</h2>
+
+![1](./flutter_01.png)
+![1](./flutter_02.png)
+![1](./flutter_03.png)
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+		MaterialApp(
+    	title: "flutter demo",
+      debugShowCheckedModeBanner: false,
+    	theme: ThemeData(
+      	// primarySwatch: Colors.deepPurple,
+        useMaterial3: true,
+        colorSchemeSeed: Colors.indigo,
+    	),
+    	home: const HomePage(),
+  	)
+	);
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        titleTextStyle: const TextStyle(
+					fontSize: 24,
+					fontWeight: FontWeight.bold
+				),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.only(
+						top: 30,
+						bottom: 0,
+						right: 10,
+						left: 10
+					),
+          child: Center(
+            child: Column(children: <Widget>[
+              TextField(
+                controller: _email,
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    labelText: "Email",
+                    hintText: "example@mail.com",
+                    border: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                    borderRadius: BorderRadius.all(
+											Radius.circular(10.0)
+                    )
+                	)
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _password,
+                autocorrect: false,
+                enableSuggestions: false,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Password",
+                  hintText: "Enter Password",
+                  border: OutlineInputBorder(
+              	    borderSide: BorderSide(),
+                	  borderRadius: BorderRadius.all(
+												Radius.circular(10.0)
+										)
+									),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+								style: ElevatedButton.styleFrom(
+									minimumSize: const Size.fromHeight(50),
+                  backgroundColor: Theme.of(context).primaryColor,
+                  foregroundColor: Colors.white,
+                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+								),
+								onPressed: () {
+									final String email = "Email: ${_email.text}";
+									final String password = "Password: ${_password.text}";
+									print(
+											"Email and Password were Submitted \n$email \n$password"
+									);
+								},
+								child: const Text(
+                  "Login",
+                )
+							),
+            ],
+					),
+        ),
+			),
+    );
+  }
+}
